88c0c56a9ece5f07eebff5372082ad4b0b85529f
ShivaHfard <sh.hfard@gmail.com>
2021-11-15 16:35:42 +0330

    Practice project Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            newGoal: ''
+        };
+    },
+    methods: {
+        addGoal() {
+            if(this.newGoal !== '')
+                this.goals.push(this.newGoal);
+            this.newGoal = '';
+        },
+        removeGoal(index) {
+            this.goals.splice(index, 1);
+        }
+    }
+});
+
+app.mount('#user-goals');
